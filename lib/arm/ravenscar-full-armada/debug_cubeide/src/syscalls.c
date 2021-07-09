41700e5679a4c11d4de0e3f316a5ae32e1c8f780
Sébastien DI MERCURIO <dimercur@insa-toulouse.fr>
2021-07-09 15:38:21 +0200

    Modif lié à l'allocation memoire entre cubeIDE et gnat

--- lib/arm/ravenscar-full-armada/debug_cubeide/src/syscalls.c
+++ lib/arm/ravenscar-full-armada/debug_cubeide/src/syscalls.c
@@ -1,47 +1,47 @@
 /**
-*****************************************************************************
-**
-**  File        : syscalls.c
-**
-**  Abstract    : System Workbench Minimal System calls file
-**
-** 		          For more information about which c-functions
-**                need which of these lowlevel functions
-**                please consult the Newlib libc-manual
-**
-**  Environment : System Workbench for MCU
-**
-**  Distribution: The file is distributed �as is,� without any warranty
-**                of any kind.
-**
-*****************************************************************************
-**
-** <h2><center>&copy; COPYRIGHT(c) 2014 Ac6</center></h2>
-**
-** Redistribution and use in source and binary forms, with or without modification,
-** are permitted provided that the following conditions are met:
-**   1. Redistributions of source code must retain the above copyright notice,
-**      this list of conditions and the following disclaimer.
-**   2. Redistributions in binary form must reproduce the above copyright notice,
-**      this list of conditions and the following disclaimer in the documentation
-**      and/or other materials provided with the distribution.
-**   3. Neither the name of Ac6 nor the names of its contributors
-**      may be used to endorse or promote products derived from this software
-**      without specific prior written permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-** SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-** CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-** OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**
-*****************************************************************************
-*/
+ *****************************************************************************
+ **
+ **  File        : syscalls.c
+ **
+ **  Abstract    : System Workbench Minimal System calls file
+ **
+ ** 		          For more information about which c-functions
+ **                need which of these lowlevel functions
+ **                please consult the Newlib libc-manual
+ **
+ **  Environment : System Workbench for MCU
+ **
+ **  Distribution: The file is distributed �as is,� without any warranty
+ **                of any kind.
+ **
+ *****************************************************************************
+ **
+ ** <h2><center>&copy; COPYRIGHT(c) 2014 Ac6</center></h2>
+ **
+ ** Redistribution and use in source and binary forms, with or without modification,
+ ** are permitted provided that the following conditions are met:
+ **   1. Redistributions of source code must retain the above copyright notice,
+ **      this list of conditions and the following disclaimer.
+ **   2. Redistributions in binary form must reproduce the above copyright notice,
+ **      this list of conditions and the following disclaimer in the documentation
+ **      and/or other materials provided with the distribution.
+ **   3. Neither the name of Ac6 nor the names of its contributors
+ **      may be used to endorse or promote products derived from this software
+ **      without specific prior written permission.
+ **
+ ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ ** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ ** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ ** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ ** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ ** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ ** SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ ** CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ ** OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **
+ *****************************************************************************
+ */
 
 /* Includes */
 #include <sys/stat.h>
@@ -97,7 +97,7 @@ int _read (int file, char *ptr, int len)
 		*ptr++ = __io_getchar();
 	}
 
-return len;
+	return len;
 }
 
 int _write(int file, char *ptr, int len)
@@ -111,28 +111,54 @@ int _write(int file, char *ptr, int len)
 	return len;
 }
 
+/* __heap_start and __heap_end are defined in the commands script for the
+   linker. They define the space of RAM that has not been allocated
+   for code or data. */
+
+extern void *__heap_start;
+extern void *__heap_end;
+
 caddr_t _sbrk(int incr)
 {
-	extern char end asm("end");
-	static char *heap_end;
-	char *prev_heap_end;
+	static void *heap_ptr = (void *)&__heap_start;
+	void *base;
+	caddr_t ret_val;
 
-	if (heap_end == 0)
-		heap_end = &end;
-
-	prev_heap_end = heap_end;
-	if (heap_end + incr > stack_ptr)
+	if (((uintptr_t)&__heap_end - (uintptr_t)heap_ptr) >= incr)
 	{
-//		write(1, "Heap and stack collision\n", 25);
-//		abort();
-		errno = ENOMEM;
-		return (caddr_t) -1;
+		base = heap_ptr;
+		heap_ptr += incr;
+		ret_val = (caddr_t)base;
+		return ret_val;
 	}
-
-	heap_end += incr;
-
-	return (caddr_t) prev_heap_end;
+	else
+	{
+		return (caddr_t)-1;
+	}
 }
+
+//caddr_t _sbrk(int incr)
+//{
+	//	extern char end asm("end");
+//	static char *heap_end;
+//	char *prev_heap_end;
+//
+//	if (heap_end == 0)
+//		heap_end = &end;
+//
+//	prev_heap_end = heap_end;
+//	if (heap_end + incr > stack_ptr)
+//	{
+////		write(1, "Heap and stack collision\n", 25);
+////		abort();
+//		errno = ENOMEM;
+//		return (caddr_t) -1;
+//	}
+//
+//	heap_end += incr;
+//
+//	return (caddr_t) prev_heap_end;
+//}
 
 int _close(int file)
 {
