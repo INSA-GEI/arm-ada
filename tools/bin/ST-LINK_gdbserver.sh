c76b984aa169487861da94027fa9b92ace84384b
vezde <dimercur@insa-toulouse.fr>
2021-11-24 18:07:30 +0100

    Correction dans le chemin de ST-LINK_gdbserver.sh

--- tools/bin/ST-LINK_gdbserver.sh
+++ tools/bin/ST-LINK_gdbserver.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-GDB_SERVER_PATH="/mnt/commetud/2eme Annee IMACS/ADA/bin"
+GDB_SERVER_PATH="/mnt/commetud/2eme Annee IMACS/ADA/tools/bin"
 echo $GDB_SERVER_PATH
 cd "$GDB_SERVER_PATH"
+
+pwd
 
 ST-LINK_gdbserver -c ./config.txt
 
