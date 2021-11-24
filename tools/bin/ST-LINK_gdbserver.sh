c5f86f10597ed4780f62fad5e36829a4bad4dcb2
vezde <dimercur@insa-toulouse.fr>
2021-11-24 11:24:43 +0100

    reference des chemins mis à jour

---
+++ tools/bin/ST-LINK_gdbserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GDB_SERVER_PATH="/mnt/commetud/2eme Annee IMACS/ADA/bin"
+echo $GDB_SERVER_PATH
+cd "$GDB_SERVER_PATH"
+
+ST-LINK_gdbserver -c ./config.txt
+
+# read -p "Press Enter to exit"
